0b3790b49eb4202ca1b964d7cda5bcb1e3bc5d3c
m2i <m2idotnet@gmail.com>
2019-06-03 11:39:45 +0200

    Container et Menu

---
+++ CoursWpfM2I/CoursWpfM2I/MenuWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoursWpfM2I
+{
+    /// <summary>
+    /// Logique d'interaction pour MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Close(object sender, EventArgs e)
+        {
+
+            //Pour fermer la fenetre
+            this.Close();
+        }
+    }
+}
