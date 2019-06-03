1692b9a26e2fe117970f7d948113d4f7a86cedd2
m2i <m2idotnet@gmail.com>
2019-06-03 11:57:34 +0200

    Binding V1

---
+++ CoursWpfM2I/CoursWpfM2I/DataBindingPersonneWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using CoursWpfM2I.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoursWpfM2I
+{
+    /// <summary>
+    /// Logique d'interaction pour DataBindingPersonneWindow.xaml
+    /// </summary>
+    public partial class DataBindingPersonneWindow : Window
+    {
+        public DataBindingPersonneWindow()
+        {
+            InitializeComponent();
+            Personne p = new Personne
+            {
+                Nom = "toto",
+                Prenom = "tata"
+            };
+            //Lier la fenetre à l'objet p 
+            DataContext = p;
+        }
+    }
+}
