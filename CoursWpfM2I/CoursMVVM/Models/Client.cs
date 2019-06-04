b1832eeff3d6819011809216a8940ee78b0851c1
m2i <m2idotnet@gmail.com>
2019-06-04 11:06:03 +0200

    Cours MVVM

---
+++ CoursWpfM2I/CoursMVVM/Models/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursMVVM.Models
+{
+    public class Client
+    {
+        private int id;
+        private string nom;
+        private string prenom;
+        private string tel;
+
+        public string Nom { get => nom; set => nom = value; }
+        public int Id { get => id; set => id = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public string Tel { get => tel; set => tel = value; }
+
+        public void Add()
+        {
+            //Ajouter dans une base de donnée
+        }
+    }
+}
---
+++ CoursWpfM2I/CoursMVVM/ViewModels/ClientsViewModel.cs
@@ -0,0 +1,73 @@
+﻿using CoursMVVM.Models;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CoursMVVM.ViewModels
+{
+    public class ClientsViewModel : ViewModelBase
+    {
+        public Client client { get; set; }
+        public string Nom
+        {
+            get
+            {
+                return client.Nom;
+            }
+            set
+            {
+                client.Nom = value;
+                //methode vient du ViewModelBase de Package MVVMLight qui permet de notifier les changement sur la props
+                RaisePropertyChanged();
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return client.Prenom;
+            }
+            set
+            {
+                client.Prenom = value;
+                RaisePropertyChanged();
+            }
+        }
+        public string Tel
+        {
+            get
+            {
+                return client.Tel;
+            }
+            set
+            {
+                client.Tel = value;
+                RaisePropertyChanged();
+            }
+        }
+        public ICommand addCommand { get; set; }
+
+        public ObservableCollection<Client> listeClients { get; set; }
+
+        public ClientsViewModel()
+        {
+            client = new Client();
+            listeClients = new ObservableCollection<Client>();
+            addCommand = new RelayCommand(AddClient);
+        }
+
+        private void AddClient()
+        {
+            client.Add();
+            listeClients.Add(client);
+        }
+
+    }
+}
